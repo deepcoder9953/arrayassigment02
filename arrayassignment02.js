de102a6069fbf682a6034536c7723a1483eb0887
deepcoder9953 <165238037+deepcoder9953@users.noreply.github.com>
2024-04-24 07:28:50 +0530

    Add files via upload

---
+++ arrayassignment02.js
@@ -0,0 +1,66 @@
+// //problem 1:-
+
+// var elements = [23, 43, 32, 33]
+// var largest = elements[1];
+// var smallest = elements[0];
+// console.log("Largest number in the array is",":-",largest,"Smallest number in the array is",":-",smallest);
+
+// //problem 2:-
+
+// var alpha = [123, 456, 789]
+// var beta = [345, 567, 678]
+// var total =  " ";
+// var length = alpha.length;
+// for(var i = 0; i<length; i++){
+//     total =+ total + alpha[i] + beta[i];
+// }
+// console.log(total);
+
+
+//problem4:-
+
+// var sample = ["nitin"];
+// var length = sample.length;
+// var pallindrome = "";
+// for(var i = 0; sample < length; i++){
+//     if(sample == length){
+//         pallindrome =+ sample[i];
+        
+//     }
+//     console.log(pallindrome);
+    
+// }
+
+//problem6:-
+
+
+// var data = [213, 3232, 4323, 53443, 53232, 53435453, 54434232]
+// var size = data.length;
+// var even = "";
+// var odd = "";
+
+// for(var i = 0; i<size; i++){
+// if(even = data[i]%2 == 0){
+//     console.log(data[i],"is even");
+// }
+// else if(odd = data[i]%3 == 0){
+//     console.log(data[i], "is odd");
+// }
+// }
+
+//problem7:-
+
+// var string = [11, 22, 19, 32, 54, 44, 65, 89]
+// var size = string.length;
+// var greatervalue = 0;
+// for(var i = 0; i < size; i++){
+//     if(greatervalue == string[i] < 50){
+//         console.log(string[i],"is greater than 50");
+//     }
+//     else if(greatervalue == string[i] > 50){
+//         console.log(string[i],"is less than 50");
+//     }
+    
+// }
+
+
